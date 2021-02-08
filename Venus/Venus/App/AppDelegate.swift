1018447be6135b6be724cb3060933b167901d241
ToriNEP <tori@nepservices.com>
2021-02-08 09:16:09 +0100

    Defined project's architecture

---
+++ Venus/Venus/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Venus
+//
+//  Created by Victoria Fargutu on 08.02.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
